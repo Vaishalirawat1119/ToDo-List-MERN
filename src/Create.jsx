1d9d93f19712546cb2310bd25c67720cecd1202a
Vaishali <rawatvaishali254@gmail.com>
2025-06-19 23:15:28 +0530

    Add task creation logic with Axios POST request

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import axios from 'axios'
 
 function Create() {
   const [task, setTask] = useState();
   const handleAdd = () => {
-    
+    axios.post('http://localhost:3000/add', {task: task})
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
   }
 
   return (
