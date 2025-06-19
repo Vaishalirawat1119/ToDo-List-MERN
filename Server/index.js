a5f9c3868426ff26563ff24b08198c7c9a35090c
Vaishali <vaishalirawat1119@gmail.com>
2025-06-19 22:44:03 +0530

    Add basic Express server setup with CORS and JSON middleware

---
+++ Server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(30001, () => {
+    console.log('Server is running on port 30001');
+})
