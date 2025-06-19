eb6eb750d845f288b5584e3e79ed66a796b2783b
Vaishali <rawatvaishali254@gmail.com>
2025-06-19 23:33:06 +0530

    Connecting database

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect('mongodb://127.0.0.1:27017/database')
 
+app.post('/add', (req, res => {
+    const task = req.body.task;
+}))
 
 app.listen(30001, () => {
     console.log('Server is running on port 30001');
