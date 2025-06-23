03eee48f5729e834ca6920a1ea8a9f3a604953a9
Vaishali <rawatvaishali254@gmail.com>
2025-06-23 20:38:10 +0530

    insalled some packages and fixed bugs

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const todoModel = require('./models/todo')
 
 const app = express();
 app.use(cors());
@@ -8,9 +9,13 @@ app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/database')
 
-app.post('/add', (req, res => {
+app.post('/add', (req, res) => {
     const task = req.body.task;
-}))
+    todoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+})
 
 app.listen(30001, () => {
     console.log('Server is running on port 30001');
