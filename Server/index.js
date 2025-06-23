bf9bde568aec5a675cecc75e35a321dca5014996
Vaishali <rawatvaishali254@gmail.com>
2025-06-23 21:58:45 +0530

    adding update and delete to app

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const todoModel = require('./models/todo')
+const TodoModel = require('./models/todo')
 
 const app = express();
 app.use(cors());
@@ -9,9 +9,29 @@ app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/database')
 
+app.get('/get', (req, res) => {
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.put('/update/:id', (req, res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndUpdate({_id: id}, {done: true})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+}) 
+
+app.delete('/delete/:id', (req, res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndDelete({_id: id})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
 app.post('/add', (req, res) => {
     const task = req.body.task;
-    todoModel.create({
+    TodoModel.create({
         task: task
     }).then(result => res.json(result))
     .catch(err => res.json(err))
