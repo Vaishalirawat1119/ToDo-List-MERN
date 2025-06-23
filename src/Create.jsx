0e021d6f7adf3c231dcb5f22ba9b5ec4adc420f7
Vaishali <rawatvaishali254@gmail.com>
2025-06-23 21:58:06 +0530

    fixed some typed mistakes and added update and delete functionality

--- src/Create.jsx
+++ src/Create.jsx
@@ -6,13 +6,15 @@ function Create() {
   const [task, setTask] = useState();
   const handleAdd = () => {
     axios.post('http://localhost:30001/add', {task: task})
-    .then(result => console.log(result))
+    .then(result => {
+      location.reload();
+    })
     .catch(err => console.log(err))
   }
 
   return (
     <div className='create_form'>
-      <input type="text" name='' id='' placeholder='Enter Task' onClick={(e) => setTask(e.target.value)}/>
+      <input type="text" placeholder='Enter Task' onChange={(e) => setTask(e.target.value)}/>
       <button type='button' onClick={handleAdd}>Add</button>
     </div>
   )
